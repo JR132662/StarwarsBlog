27d3af1cd8b73a57e35e0f148863d30ec557fc54
JR132662 <cooljon08@gmail.com>
2022-07-16 15:55:17 +0000

    save

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,9 +2,10 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
+
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<h1>Star Wars API</h1>
 		<p>
 			<img src={rigoImage} />
 		</p>
