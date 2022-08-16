2ab093bd334c694380d6aff2da88f3d4a1c1b260
JR132662 <cooljon08@gmail.com>
2022-08-16 20:03:49 +0000

    saved map function

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -5,17 +5,19 @@ import { Context } from "../store/appContext";
 
 
 
-function Card (props){
-    const {store, actions,} = useContext(context)
+function Card (props) {
+    const {store, actions,} = useContext(Context)
+    console.log(store, 'datalist')
+
     return (
         <div className="col-3 card cntainer">
-                {store.list.results &&
-            store.list.results.map((list, index) => {
+                {store.planets &&
+            store.planets.map((list, index) => {
             return(
             <div className="Card">
                 <img src="https://theforce.net/swtc/Pix/books/dk/globetatooine.jpg" className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">lol</h5>
+                    <h5 className="card-title">{list.name}</h5>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
                     <div className="card-end">
                         <Link to={`${props.routepath}${props.data.uid}`}>
